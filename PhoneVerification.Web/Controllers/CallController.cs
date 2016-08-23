7d6984ae0ec014c2c03fb463bce7b8f5ec2025af
Agustin Camino <agustin.camino@gmail.com>
2016-08-23 16:46:31 -0500

    Add call controller

---
+++ PhoneVerification.Web/Controllers/CallController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Configuration;
+using System.Web.Mvc;
+using PhoneVerification.Web.Models;
+using PhoneVerification.Web.Services;
+using Twilio;
+using Twilio.TwiML;
+using Twilio.TwiML.Mvc;
+using Number = PhoneVerification.Web.Models.Number;
+
+namespace PhoneVerification.Web.Controllers
+{
+    public class CallController : TwilioController
+    {
+        private readonly INumbersService _service;
+
+        public static string TwilioAccountSID => WebConfigurationManager.AppSettings["TwilioAccountSID"];
+        public static string TwilioAuthToken => WebConfigurationManager.AppSettings["TwilioAuthToken"];
+        public static string TwilioNumber => WebConfigurationManager.AppSettings["TwilioNumber"];
+
+        public CallController()
+        {
+            _service = new NumbersService(new PhoneVerificationDbContext());
+        }
+
+        // POST: Call/Create
+        [HttpPost]
+        public async Task<JsonResult> Create(string phoneNumber)
+        {
+            // Find and delete a phone number.
+            var number = await _service.FindByPhoneNumberAsync(phoneNumber);
+            await _service.DeleteAsync(number);
+
+            // Create a phone number.
+            var verificationCode = GenerateVerificationCode();
+            await _service.CreateAsync(
+                new Number
+                {
+                    PhoneNumber = phoneNumber,
+                    VerificationCode = verificationCode,
+                    Verified = false
+                });
+
+            // Make the phone call.
+            var client = new TwilioRestClient(TwilioAccountSID, TwilioAuthToken);
+            client.InitiateOutboundCall(new CallOptions
+            {
+                To = phoneNumber,
+                From = TwilioNumber
+            });
+
+            return Json(new {verificationCode});
+        }
+
+        // POST: Call/Webhook
+        [HttpPost]
+        public async Task<ActionResult> Webhook(string digits, string called)
+        {
+            var response = new TwilioResponse();
+
+            if (string.IsNullOrEmpty(digits))
+            {
+                response
+                    .Gather(new {numDigits = 6})
+                    .Say("Please enter your verification code");
+            }
+            else
+            {
+                var number = await _service.FindByPhoneNumberAsync(called);
+                if (digits.Equals(number.VerificationCode))
+                {
+                    response.Say("Thank you! Your phone number has been verified.");
+                }
+                else
+                {
+                    response
+                        .Gather(new { numDigits = 6 })
+                        .Say("Verification code incorrect, please try again.");
+                }
+            }
+
+            return TwiML(response);
+        }
+
+        // POST: Call/Status
+        [HttpPost]
+        public async Task<JsonResult> Status(string phoneNumber)
+        {
+            var number = await _service.FindByPhoneNumberAsync(phoneNumber);
+            var status = number.Verified ? "verified" : "unverified";
+
+            return Json(new {status});
+        }
+
+
+
+        private static string GenerateVerificationCode()
+        {
+            var code = new Random().Next(100000, 999999);
+            return code.ToString();
+        }
+    }
+}
---
+++ PhoneVerification.Web/Services/NumbersService.cs
@@ -0,0 +1,40 @@
+﻿using System.Data.Entity;
+using System.Threading.Tasks;
+using PhoneVerification.Web.Models;
+
+namespace PhoneVerification.Web.Services
+{
+    public interface INumbersService
+    {
+        Task<Number> FindByPhoneNumberAsync(string phoneNumber);
+        Task<int> CreateAsync(Number number);
+        Task<int> DeleteAsync(Number number);
+    }
+
+    public class NumbersService : INumbersService
+    {
+        private readonly PhoneVerificationDbContext _context;
+
+        public NumbersService(PhoneVerificationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Number> FindByPhoneNumberAsync(string phoneNumber)
+        {
+            return await _context.Numbers.FirstOrDefaultAsync(n => n.PhoneNumber == phoneNumber);
+        }
+
+        public async Task<int> CreateAsync(Number number)
+        {
+            _context.Numbers.Add(number);
+            return await _context.SaveChangesAsync();
+        }
+
+        public async Task<int> DeleteAsync(Number number)
+        {
+            _context.Entry(number).State = EntityState.Deleted;
+            return await _context.SaveChangesAsync();
+        }
+    }
+}
